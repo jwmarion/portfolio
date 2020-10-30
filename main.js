fb495911b6f856b7ae4e44c83a9ca3659634a895
DasomAnH <ektha116@gmail.com>
2020-10-30 16:54:26 -0400

    'commit'

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.js-nav a, .js-connect').click(function (e) {
+    e.preventDefault();
+    $('body, html').animate(
+      {
+        scrollTop: $($.attr(this, 'href')).offset().top,
+      },
+      750
+    );
+  });
+});
